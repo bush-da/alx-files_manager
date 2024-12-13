6cf9443b38ca5743338c7875f71a93ccc33a44e8
Daniel Tujuma <danielsevani12@gmail.com>
2024-12-13 21:16:58 +0300

    fix status error

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,36 +1,19 @@
+/* eslint-disable import/no-named-as-default */
 import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
 
-/**
- * Controller to handle app status and stats.
- */
-class AppController {
-  /**
-   * Returns the status of the app, including Redis and DB connectivity.
-   * @param {express.Request} req - Express request object.
-   * @param {express.Response} res - Express response object.
-   */
-  static async getStatus(req, res) {
-   console.log('Redis Status:', redisClient.isAlive());
-   console.log('DB Status:', dbClient.isAlive());
-   const status = {
-     redis: redisClient.isAlive(),
-     db: dbClient.isAlive(),
-   };
-   res.status(200).json(status);
- }
-  /**
-   * Returns the number of users and files in the database.
-   * @param {express.Request} req - Express request object.
-   * @param {express.Response} res - Express response object.
-   */
-  static async getStats(req, res) {
-    const stats = {
-      users: await dbClient.nbUsers(),
-      files: await dbClient.nbFiles(),
-    };
-    res.status(200).json(stats);
+export default class AppController {
+  static getStatus(req, res) {
+    res.status(200).json({
+      redis: redisClient.isAlive(),
+      db: dbClient.isAlive(),
+    });
+  }
+
+  static getStats(req, res) {
+    Promise.all([dbClient.nbUsers(), dbClient.nbFiles()])
+      .then(([usersCount, filesCount]) => {
+        res.status(200).json({ users: usersCount, files: filesCount });
+      });
   }
 }
-
-export default AppController;
